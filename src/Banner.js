9b7a374e38ace5750dabf6a28c71015cc9cab129
krishnakumar3 <49618916+krishnakumar3@users.noreply.github.com>
2022-03-28 20:22:08 +0530

    commit#1

---
+++ src/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './App.css';
+
+const Banner =()=>{
+    return(
+    <img src="flipkart-banner.jpg" alt="offer" height="500px " width="100%"/>
+    )
+}
+export default Banner;
